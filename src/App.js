df08b2069fcffa762b4f5a32cde7a5f06ce4959c
vladok089 <vladok.sha089@gmail.com>
2020-05-09 14:50:27 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Reactf s
         </a>
       </header>
     </div>
